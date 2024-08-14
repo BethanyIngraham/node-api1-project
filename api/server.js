3ef7aa676804eb0f726258e32e3fc63d8342e876
Bethany Ingraham <BethFIngraham@gmail.com>
2024-08-14 17:48:52 -0400

    DELETE endpoint finished

--- api/server.js
+++ api/server.js
@@ -53,4 +53,22 @@ server.get('/api/users/:id', async(req, res) => {
     }
 })
 
+server.delete('/api/users/:id', async (req, res) => {
+    try{
+        const {id} = req.params
+        const userRemoved = await User.remove(id)
+        if(!userRemoved) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }else {
+            res.status(200).json(userRemoved)
+        }
+    }catch(err) {
+        res.status(500).json({
+            message: "The user could not be removed"
+        })
+    }
+})
+
 module.exports = server // EXPORT YOUR SERVER instead of {}
