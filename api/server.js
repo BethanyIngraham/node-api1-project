0dc1d0da3ba7b4ab9c05fd3c136e245217cbd3fc
Bethany Ingraham <BethFIngraham@gmail.com>
2024-08-14 12:21:24 -0400

    server started with two get endpoints working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,38 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch(err){
+        res.status(500).json({
+            message: "The users information could not be retrieved"
+        });
+    }
+})
+
+server.get('/api/users/:id', async(req, res) => {
+    try{
+        const {id} = req.params
+        const user = await User.findById(id)
+        if(!user){
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        }else{
+            res.status(200).json(user)
+        }
+    }catch(err){
+        res.status(500).json({
+            message: "The user information could not be retrieved"
+        })
+    }
+})
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
